62bd3b7048838c2c9d105e64801bc97dede6b7e8
Jared Beale <bealej@oregonstate.edu>
2020-01-27 23:47:24 +0000

    Fixed api-client error messaging and import scope error

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,22 +1,21 @@
-import React, { Component} from 'react';
-import { LoadingIndicator, LoadingIcon,  SearchableSingleSelect } from 'lucid-ui';
-import {DataTable, Tabs, Button, Panel,CheckIcon,TextField } from 'lucid-ui';
-
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { DataTable, Button, Panel, TextField, Dialog } from 'lucid-ui';
 
 
 const style = {
   marginBottom: '10px',
 };
+
 const property = {
   hasFixedHeader: true,
-     isSelectable: false,
-     hasLightHeader: false,
-     fixedColumnCount: 1,
-     fixedRowHeight: 40,
-     isActionable: false,
+  isSelectable: false,
+  hasLightHeader: false,
+  fixedColumnCount: 1,
+  fixedRowHeight: 40,
+  isActionable: false,
+}
 
-
-  }
 class LoginPage extends Component{
   constructor(props){
     super(props);
@@ -25,96 +24,108 @@ class LoginPage extends Component{
       createdUsername: '',
       listUsername: [],
       status:'',
+      errorMessage:'',
     }
   }
+  
   componentDidMount(){
-this.getUsernames();
+    this.getUsernames();
   }
 
-getUsernames(){
-  fetch('/api/names/users', {
-method: 'GET',
-headers: {
-  'Content-Type': 'application/json',
-},
+  clearErrorMessage = () => {
+    this.setState({ errorMessage: '' });
+  }
 
-}).then((response) => response.json())
-.then((data) => {
-var listedUsernames = []
-for(var i = 0; i < data.length; i++){
-  listedUsernames.push({"username":data[i]});
-}
-
-console.log(listedUsernames);
-
-this.setState({listUsername:listedUsernames.reverse()});
-})
-.catch((error) => {
-console.error('Error:', error);
-});
-}
+  getUsernames(){
+    fetch('/api/names/users', {
+      method: 'GET',
+      headers: {
+      'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      if (!response.ok) {
+          this.setErrorMessage(response.json().content);
+      }
+      return response.json();
+    }).then((data) => {
+      var listedUsernames = []
+      for(var i = 0; i < data.length; i++){
+        listedUsernames.push({"username":data[i]});
+      }
+      this.setState({listUsername:listedUsernames.reverse()});
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      this.setState({status:error})
+    });
+  }
 
 
   signUp(){
     var data = {name: this.state.username};
-    console.log(data);
     fetch('/api/create/user', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(
-  data
-  )
-}).then((response) => response.json())
-.then((data) => {
-  console.log(data);
-  this.setState({status:data.success})
-  this.getUsernames();
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    }).then((response) => {
+        if (response.status === 400) {
+          console.log('failure');
+          response.json().then(info => this.setState({ errorMessage: info.content}));
+        }
+        else if (response.ok) {
+          console.log('success');
+          this.setState({status:data.success})
+          this.getUsernames();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({status:'Error'});
+      });
+  }
 
-})
-.catch((error) => {
-  console.error('Error:', error);
-  this.setState({status:error})
-
-});
-}
-
-
-  render(){
+  render() {
     return (
       <div>
-      <Panel>
-            <Panel.Header>
-              <strong>Please Create a Username or Log In </strong>
-            </Panel.Header>
-            <TextField
-                style={style}
-                placeholder='Username...'
-                value={this.state.username}
-                onChange={username => this.setState({ username })}
-              />
-            <Panel.Footer>
-              <Button>Log in</Button>
-              <div/>
-
-                <Button onClick={()=>this.signUp()}>Sign Up</Button>
-            </Panel.Footer>
-          </Panel>
-          <h3 style={{color: 'green'}}>{this.state.status} </h3>
-  <Panel style={{height:'500px'}}>
-
+        <Panel>
+          <Panel.Header>
+            <strong>Please Create a Username or Log In </strong>
+          </Panel.Header>
+          <TextField
+            style={style}
+            placeholder='Username...'
+            value={this.state.username}
+            onChange={username => this.setState({ username })}
+          />
+          <Panel.Footer>
+            <Button>Log in</Button>
+            <Button onClick={()=>this.signUp()}>Sign Up</Button>
+          </Panel.Footer>
+        </Panel>
+        <h3 style={{color: 'green'}}>{this.state.status}</h3>
+        <Panel style={{height:'500px'}}>
           <DataTable {...property}  className="UserTable"  data={this.state.listUsername}>
-               <DataTable.Column field='username' align='left' width={100}>
-                 Usernames
-               </DataTable.Column>
-               </DataTable>
-  </Panel>
-          </div>
-      )
+            <DataTable.Column field='username' align='left' width={100}>
+              Usernames
+            </DataTable.Column>
+          </DataTable>
+        </Panel>
+        {this.state.errorMessage && 
+          <Dialog
+            isShown='true'
+            onEscape={this.clearErrorMessage}
+            handleClose={this.clearErrorMessage}
+            Header='Error'
+            size='small'
+          >
+            {this.state.errorMessage}
+          </Dialog>
+        }
+      </div>
+    )
   }
 }
+
 export default LoginPage;
-
-
-
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router,Route, Link} from 'react-router-dom';
 import './index.css';
@@ -7,43 +7,38 @@ import LoginPage from './components/LoginPage';
 import * as serviceWorker from './serviceWorker';
 import { Tabs } from 'lucid-ui';
 import {withRouter} from 'react-router-dom';
-import "../node_modules/lucid-ui/dist/lucid.css";
 
 
 
-class App extends Component {
-
-
+class App extends React.Component {
   render(){
     return(
-    <Router>
-      <div>
-
-
-      <div class="tab">
-  <Link className="tablinks" to="/">Home</Link>
-  <Link className="tablinks"  to="/watch">Watch</Link>
-  <Link className="tablinks"  to="/login">Login</Link>
-
-      </div>
-        <Route path="/watch">
-          <VideoPage />
-        </Route>
-        <Route path="/login">
-        <LoginPage/>
-        </Route>
-        <Route exact path="/">
+      <Router>
         <div>
-      This will be the home page. Click on one of the Tabs to navigate.
-        </div>
 
-        </Route>
-      </div>
-    </Router>
+
+        <div class="tab">
+          <Link className="tablinks" to="/">Home</Link>
+          <Link className="tablinks"  to="/watch">Watch</Link>
+          <Link className="tablinks"  to="/login">Login</Link>
+        </div>
+          <Route path="/watch">
+            <VideoPage/>
+          </Route>
+          <Route path="/login">
+            <LoginPage/>
+          </Route>
+          <Route exact path="/">
+            <div>
+              This will be the home page. Click on one of the Tabs to navigate.
+            </div>
+          </Route>
+        </div>
+      </Router>
     );
   }
-
 }
+
 export default withRouter(App)
 
 ReactDOM.render(<App />, document.getElementById('root'));
