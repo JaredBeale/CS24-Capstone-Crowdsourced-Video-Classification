da52ba294046a1cf29122b34cebe6db8bb56ffc3
Sam Young <youngsam@oregonstate.edu>
2020-03-02 15:43:16 -0800

    changed banner button signout/help size

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -24,7 +24,7 @@ function Banner(props){
   function checkWatchPage(){
     if(props.location.pathname === "/watch"){
     return(
-      <Button id="color-button" style={{marginRight:'5px' }} onClick={()=>{localStorage.setItem("seenTutorial",false);props.handleShow()}}>
+      <Button id="color-button" style={{marginRight:'5px',padding: "16px" ,fontSize:"15px" }} onClick={()=>{localStorage.setItem("seenTutorial",false);props.handleShow()}}>
                Help
              </Button>)
 
@@ -46,7 +46,7 @@ function Banner(props){
 
         <div id="welcome-name"  >Welcome, {props.user}</div>
           {checkWatchPage()}
-        <Button  className="text-cell" kind='danger' onClick={()=>{
+        <Button style={{ padding: "15px" ,fontSize:"15px"}}  className="text-cell" kind='danger' onClick={()=>{
           props.setGlobalUsername("");
           props.setBannerExit(false);
       props.setBannerExit(true)
