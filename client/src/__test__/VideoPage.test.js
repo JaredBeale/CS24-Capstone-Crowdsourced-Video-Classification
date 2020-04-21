236c8094786f2bf68f7cf262911d74a1ad43b990
lozzoc <reach.maddaloc@gmail.com>
2020-04-21 14:53:31 -0700

    added test with the submit buttons on the page.

--- client/src/__test__/VideoPage.test.js
+++ client/src/__test__/VideoPage.test.js
@@ -1,11 +1,80 @@
 import React from 'react';
 import { mount, shallow, render } from 'enzyme';
-import { MemoryRouter as Router, withRouter } from 'react-router-dom' // 4.0.0
+
+import { MemoryRouter as Router} from 'react-router-dom' // 4.0.0
+import { RadioGroup  } from 'lucid-ui';
+import {Button, CheckIcon } from 'lucid-ui';
 import VideoPage from "../components/VideoPage.js"
 
 
-describe('Apps global username (not the local storage cookie) set to null', () => {
-  it('should redirect to goodbye page', () => {
+class RenderSelect extends React.Component{
+  state = { video: "https://crowd-source-circuit-tv.s3-us-west-1.amazonaws.com/dev_splits_complete/dia109_utt1.mp4",
+    videoChosen: true,
+    labelsLoaded: true,
+    LabelIndex: -1,
+    labels: ["Undecided","Undecided1","Undecided2"],
+
+  }
+  render(){
+      const listLabels = this.state.labels.map(
+        (label)=>
+          (
+            <RadioGroup.RadioButton  key={"label-"+label} >
+              <RadioGroup.Label>{label}</RadioGroup.Label>
+            </RadioGroup.RadioButton>
+          )
+        );
+      return(
+        <div id="select-div">
+          <span id="container-selction">
+            <RadioGroup
+               name='name'
+               selectedIndex={this.state.LabelIndex}
+               onSelect={this.onSelectChange}
+               style={{
+                 display: 'inline-flex',
+                 flexDirection: 'column',
+               }}
+             >
+             {listLabels}
+            </RadioGroup>
+            <span>
+              <h4 id="sessioncount">Videos classified this session: {this.state.sessionVoteCount}</h4>
+              <Button id="submit-continue-label-button" onClick={this.submitLabel}
+                isDisabled={this.state.LabelIndex===-1} size="large">
+                <CheckIcon />Save and Continue
+              </Button>
+              <Button id="submit-exit-label-button" isDisabled={this.state.LabelIndex===-1}
+                onClick={()=>{
+                  this.submitLabel();
+                  this.props.setGlobalUsername("");
+                  this.props.setBannerExit(false);
+                  this.setState({
+                    time2exit: true
+                  })
+                }} size="large">
+                <CheckIcon />Save and Exit
+              </Button>
+            </span>
+          </span>
+        </div>
+      )
+  }
+
+}
+
+/*https://enzymejs.github.io/enzyme/docs/api/ReactWrapper/setState.html
+const wrapper = mount(<Foo />);
+wrapper.setState({ name: 'bar' });
+expect(wrapper.find('.bar')).to.have.lengthOf(1);
+
+https://enzymejs.github.io/enzyme/docs/api/ShallowWrapper/dive.html
+const wrapper = shallow(<Foo />);]
+expect(wrapper.find(Bar).dive().find('.in-bar')).to.have.lengthOf(1);
+
+*/
+describe('Sign in behavior on video page', () => {
+  it('should redirect to goodbye page, when globalusername is null', () => {
     const wrapper = mount(<Router initialEntries={[ '/watch' ]}><VideoPage
                               setBannerExit={()=>{}}
                               handleShow={false}
@@ -16,6 +85,8 @@ describe('Apps global username (not the local storage cookie) set to null', () =
     expect(wrapper.find('Router')
     .prop('history').location.pathname)
     .toEqual("/goodbye")
+
+
     wrapper.unmount();
   });
 });
@@ -23,25 +94,45 @@ describe('Apps global username (not the local storage cookie) set to null', () =
 describe("Submit/continue buttons before Label cast ",()=>{
 
 
-  it('Should not be able to submit w/out labels', () => {
+  it('Form should not be present until video is chosen.', () => {
 
-    // initial render the label value is null.
-    const wrapper = shallow(<Router initialEntries={[ '/watch' ]}><VideoPage
+    // initial render the label value is null, and the buttons are not there, check.
+    const wrapper = mount(<Router initialEntries={[ '/watch' ]}><VideoPage
                               setBannerExit={()=>{}}
                               handleShow={false}
                               isShown={false}
                               setGlobalUsername={(e)=>{console.log(e)}}
                               globalUsername={"lozzoc"}/></Router>
                             );
-    const targetButton = wrapper.find("#submit-continue-label-button");
 
-    console.error("paths",targetButton.prop('history'));
-    console.log(targetButton);
-    // "submit-exit-label-button"
-    expect(wrapper.find('Router')
-    .prop('history').location.pathname)
-    .toEqual("/watch")
+
+    // wrapper.find('withRouter(VideoPage)').setState({
+    //   video: "https://crowd-source-circuit-tv.s3-us-west-1.amazonaws.com/dev_splits_complete/dia109_utt1.mp4",
+    //   videoChosen: true,
+    //   labelsLoaded: true,
+    //   labels: ["Undecided"],
+    //
+    // });
+
+    expect(wrapper.find("#submit-continue-label-button"))
+    .toEqual({});
     wrapper.unmount();
+  });
+
+
+  it('Should not be able to submit w/out a label chosen, but have watched video', () => {
+
+    const sh =  mount(<RenderSelect />)
+
+
+    // by checking the buttons props of disabled we can verify this.
+    // this is before labeling but after the video loads
+    const arr = sh.find("button").filter(function(button){
+      return button.prop("disabled") === true;
+    });
+    expect(arr.length).toEqual(sh.find("button").length);
+    // sh.unmount();
+
   });
   // expect("button")
   // .toEqual("disabled")
--- client/src/components/VideoPage.js
+++ client/src/components/VideoPage.js
@@ -82,7 +82,6 @@ class VideoPage extends Component{
   }
 
   onSelectChange(e){
-    console.log(e)
     this.setState({
       LabelIndex: e
     });
@@ -91,15 +90,18 @@ class VideoPage extends Component{
   // Ties the value of the form to the state of the class, and not
   // the radio group component. good job.
   renderSelect(){
-    const listLabels = this.state.labels.map((label)=>
-    <RadioGroup.RadioButton  key={"label-"+label} style={style}>
-   <RadioGroup.Label>{label}</RadioGroup.Label>
-   </RadioGroup.RadioButton>
-   )
+    const listLabels = this.state.labels.map(
+      (label)=>
+        (
+          <RadioGroup.RadioButton  key={"label-"+label} style={style}>
+            <RadioGroup.Label>{label}</RadioGroup.Label>
+          </RadioGroup.RadioButton>
+        )
+      );
     return(
       <div id="select-div">
-      <span id="container-selction">
-      <RadioGroup
+        <span id="container-selction">
+          <RadioGroup
              name='name'
              selectedIndex={this.state.LabelIndex}
              onSelect={this.onSelectChange}
@@ -108,31 +110,27 @@ class VideoPage extends Component{
                flexDirection: 'column',
              }}
            >
-
-        {listLabels}
-
-        </RadioGroup>
-<span>
-<h4 id="sessioncount">Videos classified this session: {this.state.sessionVoteCount}</h4>
-
-        <Button id="submit-continue-label-button" onClick={this.submitLabel}
-          isDisabled={this.state.LabelIndex===-1} size="large">
-            <CheckIcon />Save and Continue
-        </Button>
-        <Button id="submit-exit-label-button" isDisabled={this.state.LabelIndex===-1}
-          onClick={()=>{
-            this.submitLabel();
-            this.props.setGlobalUsername("");
-            this.props.setBannerExit(false);
-            this.setState({
-              time2exit: true
-            })
-          }} size="large">
-            <CheckIcon />Save and Exit
-        </Button>
-
-</span>
-</span>
+           {listLabels}
+          </RadioGroup>
+          <span>
+            <h4 id="sessioncount">Videos classified this session: {this.state.sessionVoteCount}</h4>
+            <Button id="submit-continue-label-button" onClick={this.submitLabel}
+              isDisabled={this.state.LabelIndex===-1} size="large">
+              <CheckIcon />Save and Continue
+            </Button>
+            <Button id="submit-exit-label-button" isDisabled={this.state.LabelIndex===-1}
+              onClick={()=>{
+                this.submitLabel();
+                this.props.setGlobalUsername("");
+                this.props.setBannerExit(false);
+                this.setState({
+                  time2exit: true
+                })
+              }} size="large">
+              <CheckIcon />Save and Exit
+            </Button>
+          </span>
+        </span>
       </div>
     )
   }
@@ -209,6 +207,10 @@ class VideoPage extends Component{
                       handleProgress={this.handleProgress}
                       playing={this.state.playing}/>}
 
+                  {/* In the html, the select component is not even there
+                    until the videoID/ videoURL is  chosen. which prevents malicious use
+                    of cross-site scripting i think.
+                  */}
                     {this.state.videoChosen &&
                       this.state.labelsLoaded &&
                         this.state.labels && this.renderSelect()}
@@ -334,7 +336,6 @@ this.loadVideosVoted();
     }
   }
   handleProgress(stuff){
-    console.log(stuff.played);
     if(stuff.played === 1 ){
       this.setState({
         played: stuff.played,
